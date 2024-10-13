42795c40ce9ccb48481ab4f9e5dd41aa3c2ac9fa
GIH5 <128325272+GIH5@users.noreply.github.com>
2024-10-12 20:05:22 -0400

    Update Cmod.c

--- Modules/Cmod.c
+++ Modules/Cmod.c
@@ -1,11 +1,11 @@
-#include <linux/init.h>										        /*For init and cleanup functions*/
+#include <linux/init.h>											/*For init and cleanup functions*/
 #include <linux/module.h>										/*For providing the functions and symbols*/
 #include <linux/fs.h>											/*File system usage*/
 #include <linux/cdev.h>											/*Implements the char device and functionality*/
 #include <linux/kdev_t.h>  										/*kdev_t and device is to automatically create the device file*/
-#include <linux/device.h>										
+#include <linux/device.h>										/*DO NOT TOUCH THEM PLEASE*/
 #include <linux/err.h>
-#define dev	336
+#define dev	501
 #define min	0
 
 MODULE_LICENSE("GPL");
@@ -14,6 +14,7 @@ MODULE_DESCRIPTION("Char device");
 
 static struct cdev c_cdev;
 static struct class *c_dev_class;
+/*static struct device *c_dev_crt;*/
 int ident, resp;
 
 /*********function-prototypes***********/
@@ -22,14 +23,6 @@ static ssize_t  c_write(struct file *filp, const char __user *buf, size_t len, l
 static int 	c_open(struct inode *inode, struct file *file);
 static int 	c_release(struct inode *inode, struct file *file);
 /*-------------------------------------*/
-
-static struct file_operations fops = {
-	.owner	 =	THIS_MODULE,
-	.read	 =	c_read,
-	.write	 =	c_write,
-	.open	 =	c_open,
-	.release =	c_release,
-};
 
 static int c_open(struct inode *inode, struct file *file) {
 
@@ -55,12 +48,25 @@ static int c_release(struct inode *inode, struct file *file) {
 	 return 0;
 };
 
+static struct file_operations fops = {
+	.owner	 =	THIS_MODULE,
+	.read	 =	c_read,
+	.write	 =	c_write,
+	.open	 =	c_open,
+	.release =	c_release
+};
+
 static int __init Load(void) {									       /*Check if we can staticlly assign a device number.*/
 	if (dev) {
 	   ident = MKDEV(dev, min);
 	   resp = register_chrdev_region(ident, 1, "Cmod");
-	   pr_info("Module loaded! Device identifier - MAJOR(%d):MINOR(%d)", dev, min);
 
+	   if (resp < min) {
+              pr_info("Can't register using: register_chrdev_region() - MEM_INFO: 0x%p", resp);
+	      return resp;
+	   }
+
+	   pr_info("Module loaded! Device identifier - MAJOR(%d):MINOR(%d)", dev, min);
 	   goto cdev_init;
         }
 	else {
@@ -69,9 +75,9 @@ static int __init Load(void) {									       /*Check if we can staticlly assign
 														/*If number assigned is not valid*/
 	   if (resp < min) {
 	      pr_err("Failure to allocate a device number\n Device identifier: %d", ident);
-	      return -1;
+	      return resp;
 	   }
-		
+
 	   goto cdev_init;
 	};
 
@@ -80,26 +86,40 @@ static int __init Load(void) {									       /*Check if we can staticlly assign
 		cdev_init(&c_cdev, &fops);
 		pr_info("Charecter device initiated!");
 
-		cdev_add(&c_cdev, dev, 1);
+		cdev_add(&c_cdev, ident, 1);
 		pr_info("Charecter device added");
 
-		c_dev_class = class_create(THIS_MODULE, "Cclass");						/*Create class and check /sys/class */
-		device_create(c_dev_class, NULL, dev, NULL, "Cdevice");
+		/*c_dev_class = class_create(THIS_MODULE, "Cclass");*/
+		/*c_dev_crt = device_create(c_dev_class, NULL, dev, NULL, "C-device");*/
 
-		if(IS_ERR(c_dev_class)) {
-		  pr_err("An class_create() exception was caught here");
+		if (IS_ERR(c_dev_class = class_create(THIS_MODULE, "Cclass"))) {
+		   pr_err("class_create() function error caught here!");
+		   unregister_chrdev_region(dev, 1);
+		   return -1;
+                }
+
+               if (IS_ERR(device_create(c_dev_class, NULL, dev, NULL, "C-device_1"))) {
+		  pr_err("device_create() function error caught here!");
 		  class_destroy(c_dev_class);
-		  return -1;
-		}
+               }
 
-		return 0;
+		/*
+		if (c_dev_class == NULL || c_dev_crt == NULL) {
+		   pr_err("An class_create() or device_create() exception was caught here");
+		   class_destroy(c_dev_class);
+		   return -1;
+		}
+		*/
+
+		 return 0;
+
 };
 
 static void __exit Unload(void) {
 	cdev_del(&c_cdev);
 	unregister_chrdev_region(ident, 1);
-	device_destroy(c_dev_class, dev);									/*Must have device_destroy() before class destroy to avoid a NULL pointer dereference*/
-	class_destroy(C_dev_class);
+	device_destroy(c_dev_class, dev);									/*Must have device_destroy() before class_destroy() to avoid a NULL pointer dereference*/
+	class_destroy(c_dev_class);
 	pr_info("Unloaded module");
 };
 
